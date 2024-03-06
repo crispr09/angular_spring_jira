857979a20d7a809d67bfbb90926d2290eb6dfb84
sachin <contact2sachin11@gmail.com>
2024-03-06 11:25:00 +0530

    adding project for both frontend and backend

---
+++ jira-frontend/src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { UsersComponent } from './pages/users/users.component';
+import { BoardComponent } from './pages/board/board.component';
+
+export const routes: Routes = [
+    {
+        path:'*',
+        redirectTo:'',
+        pathMatch:"full"
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"",
+        component:LayoutComponent,
+        children:[
+            {
+                path:'projects',
+                component:ProjectsComponent
+            },
+            {
+                path:'users',
+                component:UsersComponent
+            },
+            {
+                path:'board',
+                component:BoardComponent
+            }
+        ]
+
+    }
+];
